cd7e10ea5f6637c8cd7542c3ddd81b1419efd280
jeffbeckinc <56139631+jeffbeckinc@users.noreply.github.com>
2025-06-25 14:05:14 -0500

    Add files via upload

---
+++ Data Exploration.sql
@@ -0,0 +1,128 @@
+--- Check available tables in the database
+
+Select name
+from sys.tables
+
+--- Get information on individual tables  ( table name, column name, data type, length, nullable, etc) 
+EXEC sp_columns 'health-protection-coverage'
+
+--- Count number of records in 'health-protection-coverage'
+
+SELECT COUNT(*) as Total
+FROM [health-protection-coverage]
+
+--- Count missing values in specific columns
+SELECT 
+    COUNT(*) AS total_rows,
+    SUM(CASE WHEN Entity IS NULL THEN 1 
+        ELSE 0 END) AS missing_entity,
+    SUM(CASE WHEN Code IS NULL THEN 1 
+        ELSE 0 END) AS missing_Code,
+    SUM(CASE WHEN Year IS NULL THEN 1 
+        ELSE 0 END) AS missing_entity,
+    SUM(CASE WHEN [Share_of_population_covered_by_health_insurance_ILO_2014] IS NULL THEN 1 
+        ELSE 0 END) AS missing_sharepop
+FROM [health-protection-coverage];
+
+
+--- Review sample of table's data
+
+Select Top 50 *
+ FROM [health-protection-coverage]
+
+--- Update table name, field name
+
+EXEC sp_rename [health-protection-coverage], 'CountryPopInsured', --Table name changed to CountryPopInsured
+
+EXEC sp_rename 'CountryPopInsured.Share_of_population_covered_by_health_insurance_ILO_2014', 'PercentageInsured','COLUMN' -- column name updated to PercentageInsured
+
+
+--- Calculate basic statistics 
+
+SELECT 
+    MIN(PercentageInsured) AS min_PercentageInsured,
+    MAX(PercentageInsured) AS max_PercentageInsured,
+    AVG(PercentageInsured) AS avg_PercentageInsured,
+    STDEVP(PercentageInsured) AS stddev_PercentageInsured, -- standard deviation population
+	VAR(PercentageInsured) AS var_PercentageInsured -- variance
+	--SUM()
+FROM CountryPopInsured;
+
+
+--- Get distinct counts
+
+SELECT 
+COUNT(DISTINCT code) as [TotalCountries], --- number of different countries
+COUNT(DISTINCT Year) as [TotalNumberOfYears] -- number of years data was collected
+FROM CountryPopInsured
+
+
+--- Identify Top and Lowest of a category
+
+--Top 5 Country Insured Percentages
+
+SELECT Entity, PercentageInsured
+FROM CountryPopInsured
+WHERE PercentageInsured IN (
+    SELECT DISTINCT TOP 5 PercentageInsured
+    FROM CountryPopInsured
+    ORDER BY PercentageInsured DESC
+)
+ORDER BY PercentageInsured DESC;
+
+
+--Bottom 5 Country Insured Percentages
+
+SELECT Entity, PercentageInsured
+FROM CountryPopInsured
+WHERE PercentageInsured IN (
+    SELECT DISTINCT TOP 5 PercentageInsured
+    FROM CountryPopInsured
+    ORDER BY PercentageInsured ASC
+)
+ORDER BY PercentageInsured ASC;
+
+
+----- Detect outliers within 1,2,3 standard deviations
+
+-- 1 standard deviation
+SELECT *
+FROM CountryPopInsured
+WHERE PercentageInsured > (
+    SELECT AVG(PercentageInsured) + 1 * STDEV(PercentageInsured)
+    FROM CountryPopInsured
+)
+   OR PercentageInsured < (
+    SELECT AVG(PercentageInsured) - 1 * STDEV(PercentageInsured)
+    FROM CountryPopInsured
+);
+
+
+-- 2 standard deviation
+SELECT *
+FROM CountryPopInsured
+WHERE PercentageInsured > (
+    SELECT AVG(PercentageInsured) + 2 * STDEV(PercentageInsured)
+    FROM CountryPopInsured
+)
+   OR PercentageInsured < (
+    SELECT AVG(PercentageInsured) - 2 * STDEV(PercentageInsured)
+    FROM CountryPopInsured
+);
+
+
+
+-- 3 standard deviation
+SELECT *
+FROM CountryPopInsured
+WHERE PercentageInsured > (
+    SELECT AVG(PercentageInsured) + 3 * STDEV(PercentageInsured)
+    FROM CountryPopInsured
+)
+   OR PercentageInsured < (
+    SELECT AVG(PercentageInsured) - 3 * STDEV(PercentageInsured)
+    FROM CountryPopInsured
+);
+
+
+
---
+++ DataExploration part 2.sql
@@ -0,0 +1,212 @@
+----- Reviewing each table and see what for data transformations can be done, what questions we can answer with this data
+
+--- Consolidating fields into fewer tables
+
+SELECT 
+	   a.Entity
+      ,a.Code
+      ,a.Year
+      ,ROUND(a.AnnualCHE,2) AS 'CHE_PerCapita'
+	  ,b.CHE_outofGDP
+	  ,ROUND(c.AnnualChe,2) As AnnualCHE
+	  ,c.GDPperCapita
+	  ,c.EstimatedPopulation as Population
+	  ,c.Continent
+	  ,d.[MortalityRate(Under5_per1000births)] AS MortalityRate
+	  ,ROUND(e.PercentageInsured, 2) as PercentageInsured
+	  ,ROUND(f.TaxRevenuePerCapita,2) as DevelopingTaxRevenuePerCapita --- Developing Country field only
+	  ,ROUND(f.PublicExpenditurePerCapita, 2) as DevelopingPublicExpenditurePerCapita --- Developing Country field only
+	 ,ROUND(g.GDP_SpendOnHealthcare,2) as GDPSpendOnHealthcarePercent
+	 ,ROUND(h.LifeExpectancy,2) as LifeExpectancy
+     ,ROUND(i.OutOfPocket, 2) as OutOfPocket
+  FROM [Healthcare].[dbo].[AnnualCHEPerCapita] a
+    LEFT JOIN [CHE%ofGDP%] b on a.Code = b.Code and a.Year = b.Year
+    LEFT JOIN [CHE_GDP_percapita] c on a.Code = c.Code and a.Year = c.Year
+    LEFT JOIN [ChildMortalityVsCHE] d on a.Code = d.Code and a.Year = d.Year
+    LEFT JOIN [CountryPopInsured] e on a.Code = e.Code and a.Year = e.Year
+    LEFT JOIN DevelopingCountriesTaxRevenueHCSpend f on a.Code = f.Code and a.Year = f.Year
+    LEFT JOIN GDP_SpendOnHealthcare g on a.Code = g.Code and a.Year = g.Year
+    LEFT JOIN LifeExpectVSCHE h on a.Code = h.Code and a.Year = h.Year
+    LEFT JOIN OutOfPocketVS_GDP_PerCapita i on a.Code = i.Code and a.Year = i.Year
+
+
+--- After consolidating fields into on primary fact table, now we review the data to see if anything additional data cleaning can occur
+--- First, check the impact of Nulls across the table
+
+SELECT 
+    COUNT(*) AS total_rows,
+    SUM(CASE WHEN a.Entity IS NULL THEN 1 
+        ELSE 0 END) AS missing_entity,
+    SUM(CASE WHEN a.Code IS NULL THEN 1 
+        ELSE 0 END) AS missing_Code,
+    SUM(CASE WHEN a.Year IS NULL THEN 1 
+        ELSE 0 END) AS missing_year,
+    SUM(CASE WHEN a.AnnualCHE IS NULL THEN 1 
+        ELSE 0 END) AS missing_annualche,
+
+	SUM(CASE WHEN b.CHE_outofGDP IS NULL THEN 1 
+        ELSE 0 END) AS missing_che_outofGDP,
+    SUM(CASE WHEN c.AnnualChe IS NULL THEN 1 
+        ELSE 0 END) AS missing_annualche,
+    SUM(CASE WHEN c.GDPperCapita IS NULL THEN 1 
+        ELSE 0 END) AS missing_GDPperCapita,
+    SUM(CASE WHEN c.EstimatedPopulation IS NULL THEN 1 
+	    ELSE 0 END) AS missing_EstimatedPopulation,
+	
+    SUM(CASE WHEN c.Continent IS NULL THEN 1 
+        ELSE 0 END) AS missing_continent,
+    SUM(CASE WHEN d.[MortalityRate(Under5_per1000births)] IS NULL THEN 1 
+        ELSE 0 END) AS missing_mortalityrate,
+    SUM(CASE WHEN e.PercentageInsured IS NULL THEN 1 
+        ELSE 0 END) AS missing_percentageinsured,
+    SUM(CASE WHEN f.TaxRevenuePerCapita IS NULL THEN 1 
+	    ELSE 0 END) AS missing_taxrevenuepercapita,
+
+    SUM(CASE WHEN f.PublicExpenditurePerCapita IS NULL THEN 1 
+        ELSE 0 END) AS missing_PublicExpenditurePerCapita,
+ -  SUM(CASE WHEN g.GDP_SpendOnHealthcare IS NULL THEN 1 
+        ELSE 0 END) AS missing_GDP_SpendOnHealthcare,
+    SUM(CASE WHEN h.LifeExpectancy IS NULL THEN 1 
+        ELSE 0 END) AS missing_LifeExpectancy,
+    SUM(CASE WHEN i.OutOfPocket IS NULL THEN 1 
+        ELSE 0 END) AS missing_OutOfPocket
+  FROM [Healthcare].[dbo].[AnnualCHEPerCapita] a
+    LEFT JOIN [CHE%ofGDP%] b on a.Code = b.Code and a.Year = b.Year
+    LEFT JOIN [CHE_GDP_percapita] c on a.Code = c.Code and a.Year = c.Year
+    LEFT JOIN [ChildMortalityVsCHE] d on a.Code = d.Code and a.Year = d.Year
+    LEFT JOIN [CountryPopInsured] e on a.Code = e.Code and a.Year = e.Year
+    LEFT JOIN DevelopingCountriesTaxRevenueHCSpend f on a.Code = f.Code and a.Year = f.Year
+    LEFT JOIN GDP_SpendOnHealthcare g on a.Code = g.Code and a.Year = g.Year
+    LEFT JOIN LifeExpectVSCHE h on a.Code = h.Code and a.Year = h.Year
+    LEFT JOIN OutOfPocketVS_GDP_PerCapita i on a.Code = i.Code and a.Year = i.Year
+	WHERE a.Code IS NOT NULL
+
+	--- Review the missing Code and Continent values
+
+	SELECT 
+	   a.Entity
+      ,a.Code
+      ,a.Year
+      ,ROUND(a.AnnualCHE,2) AS 'CHE_PerCapita'
+	  ,b.CHE_outofGDP
+	  ,ROUND(c.AnnualChe,2) As AnnualCHE
+	  ,c.GDPperCapita
+	  ,c.EstimatedPopulation as Population
+	  ,j.Continent
+	  ,d.[MortalityRate(Under5_per1000births)] AS MortalityRate
+	  ,ROUND(e.PercentageInsured, 2) as PercentageInsured
+	  ,ROUND(f.TaxRevenuePerCapita,2) as DevelopingTaxRevenuePerCapita --- Developing Country field only
+	  ,ROUND(f.PublicExpenditurePerCapita, 2) as DevelopingPublicExpenditurePerCapita --- Developing Country field only
+	 ,ROUND(g.GDP_SpendOnHealthcare,2) as GDPSpendOnHealthcarePercent
+	 ,ROUND(h.LifeExpectancy,2) as LifeExpectancy
+     ,ROUND(i.OutOfPocket, 2) as OutOfPocket
+  FROM [Healthcare].[dbo].[AnnualCHEPerCapita] a
+    LEFT JOIN [CHE%ofGDP%] b on a.Code = b.Code and a.Year = b.Year
+    LEFT JOIN [CHE_GDP_percapita] c on a.Code = c.Code and a.Year = c.Year
+    LEFT JOIN [ChildMortalityVsCHE] d on a.Code = d.Code and a.Year = d.Year
+    LEFT JOIN [CountryPopInsured] e on a.Code = e.Code and a.Year = e.Year
+    LEFT JOIN DevelopingCountriesTaxRevenueHCSpend f on a.Code = f.Code and a.Year = f.Year
+    LEFT JOIN GDP_SpendOnHealthcare g on a.Code = g.Code and a.Year = g.Year
+    LEFT JOIN LifeExpectVSCHE h on a.Code = h.Code and a.Year = h.Year
+    LEFT JOIN OutOfPocketVS_GDP_PerCapita i on a.Code = i.Code and a.Year = i.Year
+	RIGHT JOIN DimCountry j on a.Code = j.Abbreviation
+	WHERE a.Code IS NOT NULL
+
+	-- NULL Code values are a mixture of continent, region and demographic groupings. 
+	-- Deciding to drop NULL Code values
+	-- Reviewing Continent value to see if that can be added.
+	-- Found and added Country,Abbreviation table to be able to fill out Continent
+
+
+	-- Now check Nulls after adding missing continent values
+	SELECT 
+    COUNT(*) AS total_rows,
+    SUM(CASE WHEN a.Entity IS NULL THEN 1 
+        ELSE 0 END) AS missing_entity,
+    SUM(CASE WHEN a.Code IS NULL THEN 1 
+        ELSE 0 END) AS missing_Code,
+    SUM(CASE WHEN a.Year IS NULL THEN 1 
+        ELSE 0 END) AS missing_year,
+    SUM(CASE WHEN a.AnnualCHE IS NULL THEN 1 
+        ELSE 0 END) AS missing_annualche,
+
+	SUM(CASE WHEN b.CHE_outofGDP IS NULL THEN 1 
+        ELSE 0 END) AS missing_che_outofGDP,
+    SUM(CASE WHEN c.AnnualChe IS NULL THEN 1 
+        ELSE 0 END) AS missing_annualche,
+    SUM(CASE WHEN c.GDPperCapita IS NULL THEN 1 
+        ELSE 0 END) AS missing_GDPperCapita,
+    SUM(CASE WHEN c.EstimatedPopulation IS NULL THEN 1 
+	    ELSE 0 END) AS missing_EstimatedPopulation,
+	
+    SUM(CASE WHEN j.Continent IS NULL THEN 1 
+        ELSE 0 END) AS missing_continent,
+    SUM(CASE WHEN d.[MortalityRate(Under5_per1000births)] IS NULL THEN 1 
+        ELSE 0 END) AS missing_mortalityrate,
+    SUM(CASE WHEN e.PercentageInsured IS NULL THEN 1 
+        ELSE 0 END) AS missing_percentageinsured,
+    --SUM(CASE WHEN f.TaxRevenuePerCapita IS NULL THEN 1 
+	   -- ELSE 0 END) AS missing_taxrevenuepercapita,
+
+    --SUM(CASE WHEN f.PublicExpenditurePerCapita IS NULL THEN 1 
+    --    ELSE 0 END) AS missing_PublicExpenditurePerCapita,
+ -  SUM(CASE WHEN g.GDP_SpendOnHealthcare IS NULL THEN 1 
+        ELSE 0 END) AS missing_GDP_SpendOnHealthcare,
+    SUM(CASE WHEN h.LifeExpectancy IS NULL THEN 1 
+        ELSE 0 END) AS missing_LifeExpectancy,
+    SUM(CASE WHEN i.OutOfPocket IS NULL THEN 1 
+        ELSE 0 END) AS missing_OutOfPocket
+  FROM [Healthcare].[dbo].[AnnualCHEPerCapita] a
+    LEFT JOIN [CHE%ofGDP%] b on a.Code = b.Code and a.Year = b.Year
+    LEFT JOIN [CHE_GDP_percapita] c on a.Code = c.Code and a.Year = c.Year
+    LEFT JOIN [ChildMortalityVsCHE] d on a.Code = d.Code and a.Year = d.Year
+    LEFT JOIN [CountryPopInsured] e on a.Code = e.Code and a.Year = e.Year
+    LEFT JOIN DevelopingCountriesTaxRevenueHCSpend f on a.Code = f.Code and a.Year = f.Year
+    LEFT JOIN GDP_SpendOnHealthcare g on a.Code = g.Code and a.Year = g.Year
+    LEFT JOIN LifeExpectVSCHE h on a.Code = h.Code and a.Year = h.Year
+    LEFT JOIN OutOfPocketVS_GDP_PerCapita i on a.Code = i.Code and a.Year = i.Year
+	RIGHT JOIN DimCountry j on a.Code = j.Abbreviation
+	WHERE a.Code IS NOT NULL
+
+	-- Coninent column is no longer missing any values
+	-- Ignoring missing values for taxrevenuepercapita and publicexpenditure per capita since those were only for developing countries. 
+	-- It is understood to have a lot of missing values in those columns, if it is decided to keep them in the final dataset
+	-- Reviewing percentaged insured missing values
+
+
+		SELECT 
+	   a.Entity
+      ,a.Code
+      ,a.Year
+      ,ROUND(a.AnnualCHE,2) AS 'CHE_PerCapita'
+	  ,b.CHE_outofGDP
+	  ,ROUND(c.AnnualChe,2) As AnnualCHE
+	  ,c.GDPperCapita
+	  ,c.EstimatedPopulation as Population
+	  ,j.Continent
+	  ,d.[MortalityRate(Under5_per1000births)] AS MortalityRate
+	  ,ROUND(e.PercentageInsured, 2) as PercentageInsured
+	  ,ROUND(f.TaxRevenuePerCapita,2) as DevelopingTaxRevenuePerCapita --- Developing Country field only
+	  ,ROUND(f.PublicExpenditurePerCapita, 2) as DevelopingPublicExpenditurePerCapita --- Developing Country field only
+	 ,ROUND(g.GDP_SpendOnHealthcare,2) as GDPSpendOnHealthcarePercent
+	 ,ROUND(h.LifeExpectancy,2) as LifeExpectancy
+     ,ROUND(i.OutOfPocket, 2) as OutOfPocket
+  FROM [Healthcare].[dbo].[AnnualCHEPerCapita] a
+    LEFT JOIN [CHE%ofGDP%] b on a.Code = b.Code and a.Year = b.Year
+    LEFT JOIN [CHE_GDP_percapita] c on a.Code = c.Code and a.Year = c.Year
+    LEFT JOIN [ChildMortalityVsCHE] d on a.Code = d.Code and a.Year = d.Year
+    LEFT JOIN [CountryPopInsured] e on a.Code = e.Code and a.Year = e.Year
+    LEFT JOIN DevelopingCountriesTaxRevenueHCSpend f on a.Code = f.Code and a.Year = f.Year
+    LEFT JOIN GDP_SpendOnHealthcare g on a.Code = g.Code and a.Year = g.Year
+    LEFT JOIN LifeExpectVSCHE h on a.Code = h.Code and a.Year = h.Year
+    LEFT JOIN OutOfPocketVS_GDP_PerCapita i on a.Code = i.Code and a.Year = i.Year
+	RIGHT JOIN DimCountry j on a.Code = j.Abbreviation
+	WHERE a.Code IS NOT NULL
+	AND e.PercentageInsured IS NOT NULL
+
+	-- After reviewing data, referring back to CountryPopInsured table, there is only a year value for each country, spread out over 20 years.
+	-- Table is probably better off removed and just used by itself, but will keep in dataset for time being
+	-- After reviewing those larger missing value columns, there are 4 columns out of 16 columns with largest missing value
+	-- being 175 rows or 4.7% of the data.
+	-- Considering the number of tables joined, 9 fact tables and 1 dimension table, with varying data collected across countries and years,
+	-- I feel confident about reviewing the data further in Power BI and seeing what visualizations and insights can be created
